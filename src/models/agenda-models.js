cc3a0345695f7701ce6eae26386c4f7ccd1b4363
irlaandrade <paula.irla@gmail.com>
2021-02-21 16:58:27 -0300

    Add models

---
+++ src/models/agenda-models.js
@@ -0,0 +1,18 @@
+const agenda = [
+    {
+        id: 1,
+        nome: "Carla",
+        telefone: "39833983",
+        email: "carla@gmail.com",
+        outrosTelefones: [978785252, 965653636]
+    },
+    {
+        id: 2,
+        nome: "João",
+        telefone: "37896522",
+        email: "joao@gmail.com",
+        outrosTelefones: [988552211, 902223366]
+    },
+];
+
+module.exports = agenda;
